54f1aa0977f2da40b82418329f34af43d568fa1a
aecnuxeux <99623069+aecnuxeux@users.noreply.github.com>
2022-02-13 20:17:45 -0600

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.TextBox2 = New System.Windows.Forms.TextBox
+        Me.TextBox3 = New System.Windows.Forms.TextBox
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(144, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(101, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "KM RECORRIDOS "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(121, 67)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(148, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "PAGO: Efectivo 1     Tarjeta 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(121, 102)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(153, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "VIAJE: Sencillo 1    Redondo 2"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(283, 28)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(283, 64)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(283, 99)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.SystemColors.Control
+        Me.Label4.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Red
+        Me.Label4.Location = New System.Drawing.Point(60, 150)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 16)
+        Me.Label4.TabIndex = 6
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(385, 216)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "calcular"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.ClientSize = New System.Drawing.Size(499, 262)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "EXAMEN"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,158 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim km, pago, viaje, redon As Integer
+        Dim total, total1, total2, tot, totre, puntos, pa, pare As Single
+
+        km = Val(TextBox1.Text)
+        redon = km * 2 'KILOMETROS REDONDOS
+        tot = km * 1.48 'km x precio
+        totre = redon * 1.48 'km redondos x precio
+        pago = Val(TextBox2.Text)
+        viaje = Val(TextBox3.Text)
+
+        If km > 0 Then ' KM MAYORES A 0
+
+            If pago = 1 Then ' PAGO EFECTIVO
+
+                If viaje = 1 Then 'VIAJE SENCILLO
+
+
+                    If km > 1200 Then
+                        total = tot - (tot * 0.15)
+                        puntos = km * 0.3
+                    ElseIf km > 800 Then
+                        total = tot - (tot * 0.09)
+                        puntos = km * 0.18
+                    ElseIf km > 600 Then
+                        total = tot - (tot * 0.05)
+                        puntos = km * 0.15
+                    ElseIf km > 400 Then
+                        total = tot - (tot * 0.04)
+                        puntos = km * 0.1
+                    ElseIf km <= 400 Then
+                        total = tot - (tot * 0.03)
+                        puntos = km * 0.08
+                    End If
+
+
+
+                ElseIf viaje = 2 Then 'VIAJE REDONDO
+
+
+
+                    If redon > 1200 Then
+                        total = totre - (totre * 0.2) '20% 
+                        puntos = redon * 0.3
+                    ElseIf redon > 800 Then
+                        total = (totre - (totre * 0.1)) '10% 
+                        puntos = redon * 0.25
+                    ElseIf redon > 600 Then
+                        total1 = totre - (totre * 0.05)
+                        total = total1 - (total1 * 0.07) '7% adicional
+                        puntos = redon * 0.2
+                    ElseIf redon > 400 Then
+                        total1 = totre - (totre * 0.04)
+                        total = (total1 - total1 * 0.06) '6% adicional
+                        puntos = redon * 0.15
+                    ElseIf redon <= 400 Then
+                        total1 = totre - (totre * 0.03)
+                        total = total1 - (total * 0.04) '4% adicional
+                        puntos = redon * 0.08
+                    End If
+
+
+
+                Else
+                    MsgBox("VIAJE NO VALIDO, ELIGE 1 ó 2", MsgBoxStyle.Critical)
+
+                End If ' VIN DE VIAJE
+
+
+
+            ElseIf pago = 2 Then ' PAGO TARJETA  ------ 2% ADICIONAL
+
+                pa = km * 0.02                  '  puntos adcionales 
+                pare = redon * 0.02             '  puntos adcionales en km redondos
+
+
+                If viaje = 1 Then 'VIAJE SENCILLO
+
+
+                    If km > 1200 Then
+                        total1 = tot - (tot * 0.15)
+                        total = total1 - (total1 * 0.02)
+                        puntos = (km * 0.3) + pa
+                    ElseIf km > 800 Then
+                        total1 = tot - (tot * 0.09)
+                        total = total1 - (total1 * 0.02)
+                        puntos = (km * 0.18) + pa
+                    ElseIf km > 600 Then
+                        total1 = tot - (tot * 0.05)
+                        total = total1 - (total1 * 0.02)
+                        puntos = (km * 0.15) + pa
+                    ElseIf km > 400 Then
+                        total1 = tot - (tot * 0.04)
+                        total = total1 - (total1 * 0.02)
+                        puntos = (km * 0.1) + pa
+                    ElseIf km <= 400 Then
+                        total1 = tot - (tot * 0.03)
+                        total = total1 - (total1 * 0.02)
+                        puntos = (km * 0.08) + pa
+                    End If
+
+
+
+                ElseIf viaje = 2 Then 'VIAJE REDONDO
+
+
+
+                    If redon > 1200 Then
+                        total1 = totre - (totre * 0.2) '20%
+                        total = total1 - (total * 0.02)
+                        puntos = (redon * 0.3) + pare
+                    ElseIf redon > 800 Then
+                        total1 = (totre - (totre * 0.1)) '10%
+                        total = total1 - (total * 0.02)
+                        puntos = (redon * 0.25) + pare
+                    ElseIf redon > 600 Then
+                        total1 = totre - (totre * 0.05)
+                        total2 = total1 - (total1 * 0.07) '7% adicional
+                        total = total2 - (total2 * 0.02)
+                        puntos = (redon * 0.2) + pare
+                    ElseIf redon > 400 Then
+                        total1 = totre - (totre * 0.04)
+                        total2 = (total1 - total1 * 0.06) '6% adicional
+                        total = total2 - (total2 * 0.02)
+                        puntos = (redon * 0.15) + pare
+                    ElseIf redon <= 400 Then
+                        total1 = totre - (totre * 0.03)
+                        total2 = total1 - (total * 0.04) '4% adicional
+                        total = total2 - (total2 * 0.02)
+                        puntos = (redon * 0.08) + pare
+                    End If
+
+
+
+                Else
+                    MsgBox("Viaje no valido, Elige 1 ó 2", MsgBoxStyle.Critical, " Advertencia")
+
+                End If ' VIN DE VIAJE
+
+
+
+
+            Else
+                MsgBox("Pago no Valido, Elige 1 ó 2 ", MsgBoxStyle.Critical, " Advertencia")
+
+
+            End If ' FIN DE PAGO
+
+
+        Else 'ELSE KM
+            MsgBox("Ingresa KM reales ", MsgBoxStyle.Critical, " Advertencia")
+        End If ' FIN DE KM
+        Label4.Text = "TOTAL A PAGAR:  " & total & "           PUNTOS PROXIMO VIAJE:  " & puntos
+    End Sub
+    
+End Class
